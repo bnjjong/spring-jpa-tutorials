debc8fbce0eed3860c4002cad470020a4ed2411a
Jordan <jongsang@bigin.io>
2021-11-03 12:13:38 +0900

    Initial commit

---
+++ src/main/java/io/jjongs/springjpatutorials/SpringJpaTutorialsApplication.java
@@ -0,0 +1,13 @@
+package io.jjongs.springjpatutorials;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaTutorialsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringJpaTutorialsApplication.class, args);
+  }
+
+}
